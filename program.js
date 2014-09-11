c675d79a4d7cad25c8831f7aac99f6b00d806316
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-11 01:04:55 -0500

    Exercise 5 of 13 complete, readdir async method

--- program.js
+++ program.js
@@ -1,8 +1,26 @@
 var fs = require('fs');
+var path = require('path');
 
-buf = fs.readFile(process.argv[2], 'utf8', function(err, data){
+
+// console.log(process.argv[2], process.argv[3]);
+fs.readdir(process.argv[2], function(err, files) {
   if (err) throw err;
-  var str = data.split("\n").length;
-  // Remove extra \n from end of last line
-  console.log(str-1);
+
+  var ext = "." + process.argv[3];
+  for (var i = 0; i < files.length; i++) {
+    if (ext === path.extname(files[i])) {
+      console.log(files[i]);
+    }
+  }
+
 });
+
+
+// Their solution:
+
+// fs.readdir(process.argv[2], function (err, list) {
+//       list.forEach(function (file) {
+//         if (path.extname(file) === '.' + process.argv[3])
+//           console.log(file);
+//       });
+//     });
