481f31890cd449da65cca7ba8106c9df5c3f2043
Joseph Tingsanchali <charnwoot@gmail.com>
2014-09-11 00:14:24 -0500

    Exercise 4 of 13 completed, async file i/o

--- program.js
+++ program.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 
-buf = fs.readFileSync(process.argv[2]);
-
-var str = buf.toString().split("\n").length;
-// Remove extra \n from end of last line
-console.log(str-1);
+buf = fs.readFile(process.argv[2], 'utf8', function(err, data){
+  if (err) throw err;
+  var str = data.split("\n").length;
+  // Remove extra \n from end of last line
+  console.log(str-1);
+});
